f2c9a2bddd7852bc704b9c56bdf0c30e378b657b
Joern Kiefer <jk2016@gmx.de>
2022-08-03 14:57:29 +0200

    First Version

---
+++ main.py
@@ -0,0 +1,19 @@
+myage = 45
+myage = 18
+print(myage)
+if myage>18:
+    print("Du bist erwachsen.")
+elif myage==18:
+    print("Glückwunsch, du bist gerade erwachsen geworden.")
+else:
+    print(" Bist noch ein Kind.")
+for i in range(5):
+    print(i)
+def print_account(bank_accont_info):
+    for bank_accont_info in bank_balances:
+        print(bank_accont_info)
+
+bank_balances = [1234.4,50344.00, -12.33]
+print_account(bank_balances)
+
+
